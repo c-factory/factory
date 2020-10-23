a55d4420224235b5c27950c6860b8c765d1d3ff3
Ivan Kniazkov <ivan@kniazkov.com>
2020-10-23 11:08:09 +0300

    Parsing JSON to the project_description_t struct

--- main.c
+++ main.c
@@ -1,17 +1,27 @@
 #include "strings/strings.h"
 #include "json/json.h"
+#include "allocator.h"
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef struct
+{
+    wide_string_t *name;
+} project_description_t;
+
 json_element_t * read_json_from_file(const char *file_name, bool silent_mode);
+project_description_t * parse_project_description(json_element_t *root, const char *file_name);
+void destroy_project_description(project_description_t *descr);
 
 int main(void)
 {
     json_element_t *root = read_json_from_file("factory.json", false);
     wide_string_t *js_string = json_element_to_simple_string(&root->base);
+    project_description_t * descr = parse_project_description(root, "factory.json");
     wprintf(L"%s\n", js_string->data);
     free(js_string);
     destroy_json_element(&root->base);
+    destroy_project_description(descr);
     return 0;
 }
 
@@ -48,4 +58,38 @@ json_element_t * read_json_from_file(const char *file_name, bool silent_mode)
         free(error_str);
     }
     return result;
+}
+
+project_description_t * parse_project_description(json_element_t *root, const char *file_name)
+{
+    if (root->base.type != json_object)
+    {
+        fprintf(stderr,
+            "'%s', invalid format, expected a JSON object the contains a project description", file_name);
+        return NULL;
+    }
+
+    project_description_t *descr = nnalloc(sizeof(project_description_t));
+    memset(descr, 0, sizeof(project_description_t));
+
+    json_pair_t *elem_name = get_pair_from_json_object(root->data.object, L"name");
+    if (!elem_name || elem_name->value->base.type != json_string)
+    {
+        fprintf(stderr,
+            "'%s', the project description does not contain a name", file_name);
+        goto error;
+    }
+    descr->name = duplicate_wide_string(*elem_name->value->data.string_value);
+
+    return descr;
+
+error:
+    destroy_project_description(descr);
+    return NULL;
+}
+
+void destroy_project_description(project_description_t *descr)
+{
+    free(descr->name);
+    free(descr);
 }
